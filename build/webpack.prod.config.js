64c185f610d59abd3e6b7c564ce17a9d9d64def8
zwq8299174 <zwq8299174@126.com>
2018-12-18 16:35:46 +0800

    7*

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -55,7 +55,7 @@ module.exports = merge(webpackBaseConfig, {
         // }),
         new CopyWebpackPlugin([
             {
-                from: 'td_icon.ico'
+                from: 'favicon.ico'
             },
             {
                 from: 'src/styles/fonts',
